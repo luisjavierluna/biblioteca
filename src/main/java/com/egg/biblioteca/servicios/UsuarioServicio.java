ddc3e0b5c3297e6b2be668d727322440437377f7
luisjavierluna <javierlunaluis@gmail.com>
2023-12-03 13:19:05 -0600

    V10: Update authentication methods to work with images

--- src/main/java/com/egg/biblioteca/servicios/UsuarioServicio.java
+++ src/main/java/com/egg/biblioteca/servicios/UsuarioServicio.java
@@ -1,11 +1,13 @@
 package com.egg.biblioteca.servicios;
 
+import com.egg.biblioteca.entidades.Imagen;
 import com.egg.biblioteca.entidades.Usuario;
 import com.egg.biblioteca.enumeraciones.Rol;
 import com.egg.biblioteca.excepciones.MiException;
 import com.egg.biblioteca.repositorios.UsuarioRepositorio;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Optional;
 import javax.servlet.http.HttpSession;
 import javax.transaction.Transactional;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -19,6 +21,7 @@ import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
 import org.springframework.web.context.request.RequestContextHolder;
 import org.springframework.web.context.request.ServletRequestAttributes;
+import org.springframework.web.multipart.MultipartFile;
 
 @Service
 public class UsuarioServicio implements UserDetailsService {
@@ -26,8 +29,11 @@ public class UsuarioServicio implements UserDetailsService {
     @Autowired
     private UsuarioRepositorio usuarioRepositorio;
     
+    @Autowired
+    private ImagenServicio imagenServicio;
+    
     @Transactional
-    public void registrar(String nombre, String email, String password, String password2) throws MiException {
+    public void registrar(MultipartFile archivo,  String nombre, String email, String password, String password2) throws MiException {
         validar(nombre, email, password, password2);
 
         Usuario usuario = new Usuario();
@@ -38,8 +44,43 @@ public class UsuarioServicio implements UserDetailsService {
         usuario.setPassword(new BCryptPasswordEncoder().encode(password));
 
         usuario.setRol(Rol.USER);
+        
+        Imagen imagen = imagenServicio.guardar(archivo);
+        
+        usuario.setImagen(imagen);
      
         usuarioRepositorio.save(usuario);
+    }
+    
+    @Transactional
+    public void actualizar(MultipartFile archivo, String idUsuario, String nombre, String email, String password, String password2) throws MiException {
+
+        validar(nombre, email, password, password2);
+
+        Optional<Usuario> respuesta = usuarioRepositorio.findById(idUsuario);
+        if (respuesta.isPresent()) {
+
+            Usuario usuario = respuesta.get();
+            usuario.setNombre(nombre);
+            usuario.setEmail(email);
+
+            usuario.setPassword(new BCryptPasswordEncoder().encode(password));
+
+            usuario.setRol(Rol.USER);
+            
+            String idImagen = null;
+            
+            if (usuario.getImagen() != null) {
+                idImagen = usuario.getImagen().getId();
+            }
+            
+            Imagen imagen = imagenServicio.actualizar(archivo, idImagen);
+            
+            usuario.setImagen(imagen);
+            
+            usuarioRepositorio.save(usuario);
+        }
+
     }
     
     private void validar(String nombre, String email, String password, String password2) throws MiException {
