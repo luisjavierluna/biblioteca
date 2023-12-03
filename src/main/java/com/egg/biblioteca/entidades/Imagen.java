72ebb7ef093a7781794ff0efd079753dc399bbc6
luisjavierluna <javierlunaluis@gmail.com>
2023-12-03 10:37:52 -0600

    V9: Add methods to handle images

---
+++ src/main/java/com/egg/biblioteca/entidades/Imagen.java
@@ -0,0 +1,29 @@
+package com.egg.biblioteca.entidades;
+
+import java.io.Serializable;
+import javax.persistence.Basic;
+import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.Lob;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.hibernate.annotations.GenericGenerator;
+
+@Entity
+@Data @NoArgsConstructor
+public class Imagen implements Serializable {
+    
+    @Id
+    @GeneratedValue(generator = "uuid")
+    @GenericGenerator(name = "uuid", strategy = "uuid2")
+    private String id;
+    
+    private String mime;
+    
+    private String nombre;
+    
+    @Lob @Basic(fetch = FetchType.LAZY)
+    private byte[] contenido;
+}
--- src/main/java/com/egg/biblioteca/entidades/Usuario.java
+++ src/main/java/com/egg/biblioteca/entidades/Usuario.java
@@ -7,6 +7,7 @@ import javax.persistence.EnumType;
 import javax.persistence.Enumerated;
 import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
+import javax.persistence.OneToOne;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -27,5 +28,8 @@ public class Usuario implements Serializable {
     
     @Enumerated(EnumType.STRING)
     private Rol rol;
+    
+    @OneToOne
+    private Imagen imagen;
     
 }
