f7db91094da65daad54fa541a3ccec166d3936d2
luisjavierluna <javierlunaluis@gmail.com>
2023-12-05 10:20:24 -0600

    Add main entities

---
+++ src/main/java/com/egg/biblioteca/entidades/Libro.java
@@ -0,0 +1,34 @@
+package com.egg.biblioteca.entidades;
+
+import java.io.Serializable;
+import java.util.Date;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.ManyToOne;
+import javax.persistence.Temporal;
+import javax.persistence.TemporalType;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Data @NoArgsConstructor @AllArgsConstructor
+public class Libro implements Serializable {
+    
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long isbn;
+    private String titulo;
+    private Integer ejemplares;
+    
+    @Temporal(TemporalType.DATE)
+    private Date date;
+    
+    @ManyToOne // Muchos libros tienen un mismo autor
+    private Autor autor;
+    
+    @ManyToOne // Muchos libren tienen una misma editorial
+    private Editorial editorial;
+}
